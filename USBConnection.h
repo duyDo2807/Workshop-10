2befb20372bd16ca0d20e9e19197a9d996b8bd4a
duyDo2807 <a1881078@adelaide.edu.au>
2023-10-09 23:40:50 +1030

    Workshop 10

---
+++ USBConnection.h
@@ -0,0 +1,28 @@
+#ifndef USBCONNECTION_H
+#define USBCONNECTION_H
+
+class USBConnection {
+ private:
+  int ID;
+  USBConnection(int id) : ID(id) {}
+
+ public:
+  static std::stack<int> ids;
+
+  static USBConnection* CreateUsbConnection() {
+    if (!ids.empty()) {
+      int id = ids.top();
+      ids.pop();
+      return new USBConnection(id);
+    }
+    return nullptr;
+  }
+
+  ~USBConnection() { ids.push(ID); }
+
+  int get_id() const { return ID; }
+};
+
+std::stack<int> USBConnection::ids({3, 2, 1});
+
+#endif
